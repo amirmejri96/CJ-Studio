c2b00e99ecb8e219b2f8746b54eeef9514a8c20f
amirmejri96 <amirmejri1996@gmail.com>
2025-02-24 14:42:34 +0100

    works

--- src/pages/Works/Works.js
+++ src/pages/Works/Works.js
@@ -3,7 +3,21 @@ import './Works.css'
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <main id='Projects' className='project'>
+      <h3>Projects</h3>
+      <div className='project-content'>
+      <section className='flex left-section'>
+        <button className='active'>All Projects</button>
+        <button>Fashions</button>
+        <button>Events</button>
+        <button>Weddings</button>
+        <button>Other Projects</button>
+      </section>
+      <section className='right-section'>
+
+      </section>
+      </div>
+    </main>
   )
 }
 
